2245285bbd9d103df233cb15b6b28e73f8b05d14
Edheltur <kanaroooo@gmail.com>
2017-11-29 20:45:31 +0500

    Базовая имлементация

--- async.js
+++ async.js
@@ -3,11 +3,43 @@
 exports.isStar = true;
 exports.runParallel = runParallel;
 
+function withTimeout(job, timeout) {
+    return Promise.race([
+        new Promise(resolve => setTimeout(resolve, timeout,
+            new Error(`Timed out waiting for ${timeout} MILLISECONDS`))),
+        job().catch(error => error)
+    ]);
+}
+
 /** Функция паралелльно запускает указанное число промисов
  * @param {Array} jobs – функции, которые возвращают промисы
  * @param {Number} parallelNum - число одновременно исполняющихся промисов
  * @param {Number} timeout - таймаут работы промиса
+ * @returns {Promise<Array>}
  */
 function runParallel(jobs, parallelNum, timeout = 1000) {
-    // асинхронная магия
+    let results = [];
+    let lastBatchId = 0;
+
+    function executeBatch(batchJobs) {
+        if (batchJobs.length === 0) {
+            return Promise.resolve();
+        }
+
+        const batchId = lastBatchId++;
+        const currentJob = withTimeout(batchJobs.shift(), timeout);
+
+        return currentJob.then((result) => {
+            results[batchId] = result;
+
+            return executeBatch(batchJobs);
+        });
+    }
+
+    let batches = [];
+    while (parallelNum-- > 0 && jobs.length > 0) {
+        batches.push(executeBatch(jobs));
+    }
+
+    return Promise.all(batches).then(() => results);
 }
